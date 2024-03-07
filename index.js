1c749130128fba5579c28ef5374f99a75c54b6d4
pratham9467 <76515135+pratham9467@users.noreply.github.com>
2024-03-07 05:32:47 +0530

    files

---
+++ index.js
@@ -0,0 +1,42 @@
+function cursoreffect() {
+  var page1Content = document.querySelector(".page1Content");
+  var playReel = document.querySelector(".playReel");
+
+  page1Content.addEventListener("mousemove", function (e) {
+    gsap.to(playReel, {
+      x: e.x,
+      y: e.y,
+    });
+  });
+  page1Content.addEventListener("mouseenter", function () {
+    gsap.to(playReel, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+
+  page1Content.addEventListener("mouseleave", function () {
+    gsap.to(playReel, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+}
+
+cursoreffect();
+
+function page2Animation() {
+  gsap.from(".elem h1", {
+    y: 120,
+    stagger: 0.2,
+    durtion: 1,
+    scrollTrigger: {
+      trigger: ".page2",
+      scroll: "body",
+      start: "top 47%",
+      end: "top 46%",
+      marker: true,
+      scrub: 2,
+    },
+  });
+};
